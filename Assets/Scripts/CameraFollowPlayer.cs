9954c6499498156a32c0ff6466fe20260d0b22ac
szwos <szwos5@gmail.com>
2023-07-22 15:17:23 +0200

    dynamic camera size

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour
 {
+
+    [SerializeField]
+    public Rigidbody2D playerRigidbody;
     public Transform CameraTransform;
     public Transform CameraTarget;
+    public Camera camera;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        
         CameraTransform = GetComponent<Transform>();
+        camera = GetComponent<Camera>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         CameraTransform.position = new Vector3(CameraTarget.position.x, CameraTarget.position.y, CameraTransform.position.z);
+        camera.orthographicSize = Mathf.Sqrt(playerRigidbody.velocity.magnitude) + 3;
     }
 }
